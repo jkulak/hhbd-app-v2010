c88f80f8e276a8289d8034e579c1137b697c0bbd
Jakub.kulak@gmail.com <Jakub.kulak@gmail.com@6c0f084b-7910-9a3f-d28b-cafd93553588>
2010-11-24 03:12:44 +0000

    creating tag v0.12

---
+++ tags/v0.12/application/models/Label/Container.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Label
+ *
+ * @author Kuba
+ * @version $Id$
+ * @copyright __MyCompanyName__, 11 October, 2010
+ * @package default
+ **/
+
+class Model_Label_Container
+{
+  
+  public $id;
+  public $name;
+    
+  function __construct($params, $full = false)
+  {
+    $this->id = $params['id'];
+    $this->name = $params['name'];
+  }
+  
+  public function url($canonical = false)
+  {
+    return Jkl_Tools_Url::createUrl($this->name);
+    // return $this->title;
+  }
+}
