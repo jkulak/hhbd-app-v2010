04c2f1e1fa4cac4a804f4dd29fb8e7221b3816c5
Jakub.kulak@gmail.com <Jakub.kulak@gmail.com@6c0f084b-7910-9a3f-d28b-cafd93553588>
2010-12-09 21:31:00 +0000

    creating tag v0.29

---
+++ tags/v0.29/public/js/s.js
@@ -0,0 +1,40 @@
+// on load create a script to load a final script here
+
+$(function(){
+  $("span.toggle > a").toggle(
+    function () {
+      $(this).text("Pokaż szczegóły");
+      $("ul.feat").addClass("hidden");        
+    },
+    function () {
+      $(this).text("Ukryj szczegóły");
+      $("ul.feat").removeClass("hidden");
+    }
+  );
+  
+  $("#q").focus(function(){
+    if($(this).text() == "Szukaj...") $(this).text("")
+  });
+  $("#q").focus();
+
+  $('table tr').hover(
+     function() {
+      $(this).toggleClass('zebra');
+     }
+  );
+  
+  $('#rateUp').click(function() {
+    $('#upCount').text(parseInt($('#upCount').text())+1);
+  });
+
+  $('#rateDown').click(function() {
+    $('#downCount').text(parseInt($('#downCount').text())+1);
+  });
+  
+  // unhide javascript functionality
+  $('.jsHidden').show();
+  
+  $('.play').hover(function() {
+    $(this).toggleClass('playM');
+  });
+});
