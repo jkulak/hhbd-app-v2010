987d42ae094a9a74c2046cee08a6684b60c9f6a5
Jakub.kulak@gmail.com <Jakub.kulak@gmail.com@6c0f084b-7910-9a3f-d28b-cafd93553588>
2010-12-15 01:30:41 +0000

    creating tag v0.32

---
+++ tags/v0.32/application/models/Label/Api.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * Label Api
+ *
+ * @author Kuba
+ * @version $Id$
+ * @copyright __MyCompanyName__, 12 October, 2010
+ * @package hhbd
+ **/
+
+class Model_Label_Api extends Jkl_Model_Api
+{
+  static private $_instance;
+  
+  /**
+   * Singleton instance
+   *
+   * @return Model_Label_Api
+   */
+  public static function getInstance()
+  {
+      if (null === self::$_instance) {
+          self::$_instance = new self();
+      }
+      return self::$_instance;
+  }
+
+  /**
+   * Creates object and fetches the list from database result
+   */
+  public function getList($query)
+  {
+    $result = $this->_db->fetchAll($query);
+    $list = new Jkl_List(); 
+    foreach ($result as $params) {
+      $list->add(new Model_Label_Container($params));
+    }
+    return $list;
+  }
+  
+  public function find($id, $full = false)
+  {
+    $id = intval($id);
+    $query = 'select *, id AS lab_id from labels where id=' . $id;
+    $result = $this->_db->fetchAll($query);
+    $item = new Model_Label_Container($result[0], $full);
+    return $item;
+  }
+  
+  public function getFullList()
+  {
+    $query = "SELECT t1.id AS lab_id, t1.`name`, t1.`website`, count(t2.id) AS album_count
+              FROM labels t1, albums t2
+              WHERE (t1.id!=27 AND t2.`labelid`=t1.id)
+              GROUP BY t1.`id`
+              ORDER BY t1.`name`";
+    return $this->getList($query);
+  }
+  
+  public function getWithMostAlbums($limit = 10)
+  {
+    $limit = intval($limit);
+    $query = "SELECT count(t2.id) AS album_count, t1.`id` AS lab_id, t1.`name`
+              FROM labels t1, albums t2
+              WHERE (t2.`labelid`=t1.`id` AND t1.`id`<>27)
+              GROUP BY t1.`id`
+              ORDER BY album_count DESC" . 
+              (isset($limit)?" LIMIT $limit":'');
+              
+    return $this->getList($query);
+  }
+  
+  public function getLike($like, $limit = 10, $page = 1)
+  {
+    $like = Jkl_Db::escape($like);
+    $limit = intval($limit);
+    $page = intval($page - 1);
+    $page = ($page<1)?0:$page;
+    
+    $query = "SELECT count(t2.id) AS album_count, t1.`id` AS lab_id, t1.`name`
+              FROM labels t1, albums t2
+              WHERE (t2.`labelid`=t1.`id` AND t1.`id`<>27 AND t1.`name` LIKE '%$like%')
+              GROUP BY t1.`id`
+              ORDER BY t1.`viewed` DESC" . 
+              (($limit != null)?' LIMIT ' . $limit:'') . 
+              ' OFFSET ' . ($page*$limit);
+              
+    return $this->getList($query);
+  }
+  
+  public function getLikeCount($like = '')
+  {
+    $like = Jkl_Db::escape($like);
+    $query = "SELECT count(*) as count
+              FROM labels AS t1
+              WHERE t1.name LIKE '%$like%'"; 
+    $result = $this->_db->fetchAll($query);
+    return intval($result[0]['count']);
+  }
+}
---
+++ tags/v0.32/public/js/s.js
@@ -0,0 +1,52 @@
+// on load create a script to load a final script here
+
+$(function(){
+  // toggle tracklist additional information
+  $("#tracklist span.toggle > a").toggle(
+    function () {
+      $(this).text("Pokaż szczegóły");
+      $("ul.feat").addClass("hidden");        
+    },
+    function () {
+      $(this).text("Ukryj szczegóły");
+      $("ul.feat").removeClass("hidden");
+    }
+  );
+  
+  // toggle view/hide autoDescription
+  $("#description span.toggle > a").toggle(
+    function () {
+      $(this).text("Ukryj opis standardowy");
+      $("p.auto").removeClass("hidden");
+    },
+    function () {
+      $(this).text("Pokaż opis standardowy");
+      $("p.auto").addClass("hidden");        
+    }
+  );
+  
+  $("#q").focus(function(){
+    if($(this).text() == "Szukaj...") $(this).text("")
+  });
+  // $("#q").focus();
+
+  $('table tr').hover(
+     function() {
+      $(this).toggleClass('zebra');
+     }
+  );
+  
+  $('#rateUp').click(function() {
+    $('#upCount').text(parseInt($('#upCount').text())+1);
+  });
+
+  $('#rateDown').click(function() {
+    $('#downCount').text(parseInt($('#downCount').text())+1);
+  });
+  
+  // unhide javascript functionality
+  $('.jsHidden').show();
+  
+  $('*').text($(this).html());
+  
+});
