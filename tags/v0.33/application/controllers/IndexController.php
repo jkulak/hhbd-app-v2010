2d8e64c1b5bc4c11cf4d4694fffb9c9790515157
Jakub.kulak@gmail.com <Jakub.kulak@gmail.com@6c0f084b-7910-9a3f-d28b-cafd93553588>
2010-12-16 01:50:11 +0000

    creating tag v0.33

---
+++ tags/v0.33/application/controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+class IndexController extends Zend_Controller_Action
+{
+    public function init()
+    {
+      $this->view->headMeta()->setName('keywords', 'polski hip-hop, albumy');
+      $this->view->headTitle()->headTitle('Hhbd.pl - Hip-hopowa baza danaych', 'SET');
+      $this->view->headMeta()->setName('description', 'Albumy w hhbd.pl');
+    }
+
+    public function indexAction()
+    {
+      $this->view->newestList = Model_Album_Api::getInstance()->getNewest(3);
+      $this->view->announcedList = Model_Album_Api::getInstance()->getAnnounced(3);
+    }
+    
+    public function aboutUsAction()
+    {
+    }
+}
---
+++ tags/v0.33/public/js/s.js
@@ -0,0 +1,87 @@
+// on load create a script to load a final script here
+
+
+
+function getCookie (name) {
+    var dc = document.cookie;
+    var cname = name + "=";
+
+    if (dc.length > 0) {
+      begin = dc.indexOf(cname);
+      if (begin != -1) {
+        begin += cname.length;
+        end = dc.indexOf(";", begin);
+        if (end == -1) end = dc.length;
+        return unescape(dc.substring(begin, end));
+        }
+      }
+    return null;
+}
+// cookies handling
+function setCookie(name, value, expires) {
+  document.cookie = name + "=" + escape(value) + "; path=/" + ((expires == null) ? "" : "; expires=" + expires.toGMTString());
+}
+
+
+
+var exp = new Date();
+exp.setTime(exp.getTime() + (1000 * 60 * 60 * 24 * 350));
+
+$(function(){
+  // toggle tracklist additional information
+  $("#tracklist span.toggle > a").toggle(
+    function () {
+      $(this).text("Pokaż szczegóły");
+      $("ul.feat").hide();
+      setCookie('albumShowDetails', 0, exp);   
+    },
+    function () {
+      $(this).text("Ukryj szczegóły");
+      $("ul.feat").show();
+      setCookie('albumShowDetails', 1, exp);
+    }
+  );
+  
+  // toggle view/hide autoDescription
+  $("#description span.toggle > a").toggle(
+    function () {
+      $(this).text("Ukryj opis standardowy");
+      $("p.auto").show();
+      setCookie('albumShowAuto', 1, exp);
+    },
+    function () {
+      $(this).text("Pokaż opis standardowy");
+      $("p.auto").hide();
+      setCookie('albumShowAuto', 0, exp);
+    }
+  );
+  
+  $("#q").focus(function(){
+    if($(this).text() == "Szukaj...") $(this).text("")
+  });
+  // $("#q").focus();
+
+  $('table tr').hover(
+     function() {
+      $(this).toggleClass('zebra');
+     }
+  );
+  
+  $('#rateUp').click(function() {
+    $('#upCount').text(parseInt($('#upCount').text())+1);
+  });
+
+  $('#rateDown').click(function() {
+    $('#downCount').text(parseInt($('#downCount').text())+1);
+  });
+  
+  // unhide javascript functionality, and hide what can be revelaed using js
+  $('.js-visible').show();
+  $('.js-hidden').hide();
+  
+
+  if (getCookie('albumShowDetails') == 0) { $("#tracklist span.toggle > a").click(); };
+  if (getCookie('albumShowAuto') == 1) { $("#description span.toggle > a").click(); };
+
+  
+});
