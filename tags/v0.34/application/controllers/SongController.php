f29ed5a5b6bd5f78aa2658409116366e91b27cab
Jakub.kulak@gmail.com <Jakub.kulak@gmail.com@6c0f084b-7910-9a3f-d28b-cafd93553588>
2010-12-19 21:31:37 +0000

    creating tag v0.34

---
+++ tags/v0.34/application/controllers/SongController.php
@@ -0,0 +1,38 @@
+<?php
+
+class SongController extends Zend_Controller_Action
+{
+  
+  public function init()
+  {
+    $this->view->headMeta()->setName('keywords', 'polski hip-hop, albumy');
+    $this->view->headTitle()->headTitle('Piosenki', 'PREPEND');
+    $this->view->headMeta()->setName('description', 'Piosenki, teksty, teledyski w hhbd.pl');
+    $this->params = $this->getRequest()->getParams();
+  }
+
+  public function indexAction()
+  {
+  }
+  
+  public function viewAction()
+  {
+    // content
+    $params = $this->getRequest()->getParams();
+    $this->view->song = Model_Song_Api::getInstance()->find($params['id'], true);
+    
+    // sidenotes
+    $albumSongs = array();
+    foreach ($this->view->song->featured->items as $key => $value) {
+      $albumSongs[] = Model_Song_Api::getInstance()->getTracklist($value->id, null);
+    }
+    $this->view->albumSongs = $albumSongs;
+    $this->view->popularSongs = Model_Song_Api::getInstance()->getMostPopular(15);
+    $this->view->autoPlay = isset($this->params['autoplay']);
+
+    // seo meta
+    $this->view->headTitle()->set($this->view->song->albumArtist->name . ' - ' . $this->view->song->title . ' tekst, teledysk, sample');
+        $this->view->headMeta()->setName('keywords', $this->view->song->albumArtist->name . ',' . $this->view->song->title . ',tekst,teledysk,sample');
+    $this->view->headMeta()->setName('description', $this->view->song->albumArtist->name . ' - ' . $this->view->song->title . ', tekst, teledysk, premiera, informacje o samplach i inne ciekawe informacje o piosence na największej polskiej stronie o hip-hopie.');
+  }
+}
