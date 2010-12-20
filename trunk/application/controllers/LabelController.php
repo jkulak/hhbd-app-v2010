5f87ededa463968e5a5d1aa7fe298aca0dda92aa
Jakub.kulak@gmail.com <Jakub.kulak@gmail.com@6c0f084b-7910-9a3f-d28b-cafd93553588>
2010-12-20 02:11:46 +0000

    moved song and label description autogeneration to controllers

--- trunk/application/controllers/LabelController.php
+++ trunk/application/controllers/LabelController.php
@@ -31,18 +31,45 @@ class LabelController extends Zend_Controller_Action
     $params = $this->getRequest()->getParams();
     $label = Model_Label_Api::getInstance()->find($params['id'], true);
     $label->releases = Model_Album_Api::getInstance()->getLabelReleases($label->id, null);
-    $this->view->label = $label;
-    $this->view->withMostAlbums = Model_Label_Api::getInstance()->getWithMostAlbums();
-    
     $artists = array();
     foreach ($label->releases->items as $key => $value) {
       $artists[] = $value->artist->name;
     }
-    $artists = array_unique($artists);
-    $this->view->label->artists = implode(', ', $artists);
+    $label->artists = implode(', ', array_unique($artists));
+    $label->autoDescription = $this->_generateDescription($label);
+    $this->view->label = $label;
+  
+    $this->view->withMostAlbums = Model_Label_Api::getInstance()->getWithMostAlbums();
     
     $this->view->headTitle()->set($label->name . ' - Hhbd.pl');
     $this->view->headMeta()->setName('keywords', $label->name . ',' . implode(',', $artists) . ',wytwórnia,polski hip-hop');
     $this->view->headMeta()->setName('description', $label->name . ' to wytwórnia wydająca polski hip-hop, w jej szeregach są tacy artyści jak: ' . implode(', ', $artists));
+  }
+  
+  // description autogeneration, displayedfor SEO purposes
+  private function _generateDescription($label)
+  {
+    $description = '';
+    
+    $description .= $label->name . ' wydała do tej pory ' . count($label->releases->items) . ' ';
+    $description .= ((count($label->releases->items)>4)?'albumów':((count($label->releases->items)>1)?'albumy':'album')) . '. ';
+    $description .= 'Wytwórnia wydaje taki artystów jak: ' . $label->artists . '. ';
+    $description .= 'Najnowsze wydawnictwo wytwórni to: ' . $label->releases->items[0]->title . ', ';
+    $description .= 'za które odpowiedizalny jest ' . $label->releases->items[0]->artist->name . '. ';
+    $description .= 'Ostatni album ' . $label->name . ' wydało ' . $label->releases->items[0]->releaseDateNormalized . '. ';
+    
+    if (!empty($label->website)) {
+      $description .= 'Oficjalna strona wytwórni, to: ' . $label->website . ' - sprawdzajcie! ';
+    }
+    
+    if (!empty($label->email)) {
+      $description .= 'Jeżeli chcecie się skontaktować bezpośrednio z wytwórnią, to piszcie na adres: ' . $label->email . '. ';
+    }
+    
+    if (!empty($label->addres)) {
+      $description .= 'Pocztowy adres wytwórni to: ' . $label->addres . ' - możecie słać demówki!';
+    }
+    
+    return $description;
   }
 }
