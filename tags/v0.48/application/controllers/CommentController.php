3dc12baf95f9ee30aa5f59db253b06320bb9452a
Jakub.kulak@gmail.com <Jakub.kulak@gmail.com@6c0f084b-7910-9a3f-d28b-cafd93553588>
2010-12-28 01:48:52 +0000

    creating tag v0.48

---
+++ tags/v0.48/application/controllers/CommentController.php
@@ -0,0 +1,85 @@
+<?php
+
+class CommentController extends Zend_Controller_Action
+{
+  
+  public function init()
+  {
+    $this->params = $this->getRequest()->getParams();
+  }
+
+  public function indexAction()
+  {
+    // print_r($this->params);
+    $content = htmlentities($this->params['content'], ENT_COMPAT, "UTF-8");
+    $author = htmlentities('~' . $this->params['author'], ENT_COMPAT, "UTF-8");
+    $authorIp = $_SERVER['REMOTE_ADDR'];
+    $objectId = $this->params['com_object_id'];
+    $objectType = $this->params['com_object_type'];
+
+    switch ($this->params['com_object_type']) {
+      case 'a':
+        $data = Model_Album_Api::getInstance()->redirectById($this->params['com_object_id']);
+        $redirect = Jkl_Tools_Url::createUrl($data['art_name'] . '+-+' . $data['alb_title'] . '-a' . $data['alb_id'] . '.html');
+        break;
+        
+      case 'p':
+      case 'wykonawca':
+        $data = Model_Artist_Api::getInstance()->redirectById($this->params['com_object_id']);
+        $redirect = Jkl_Tools_Url::createUrl($data['art_name'] . '-p' . $data['art_id'] . '.html');
+        break;
+      
+      case 'l':
+      case 'wytwornia':
+        $data = Model_Label_Api::getInstance()->redirectById($this->params['com_object_id']);
+        $redirect = Jkl_Tools_Url::createUrl($data['lab_name'] . '-l' . $data['lab_id'] . '.html');
+        break;
+      
+      case 's':
+      case 'utwor':
+        $data = Model_Song_Api::getInstance()->redirectById($this->params['com_object_id']);
+        $redirect = Jkl_Tools_Url::createUrl($data['sng_title'] . '-s' . $data['sng_id'] . '.html');
+        break;
+        
+      case 'n':  
+      case 'news':
+        $data = Model_News_Api::getInstance()->redirectById($this->params['com_object_id']);
+        $redirect = Jkl_Tools_Url::createUrl($data['nws_title'] . '-n' . $data['nws_id'] . '.html');
+        break;
+  
+      default:
+        break;
+    }
+    
+    if (empty($content)) {
+      $redirect .= '?postError=1&emptyComment=1#comments';
+      header("Location: /" . str_replace(' ', '+', $redirect));
+      exit();
+    }
+    
+    if (strlen($content) > 1000) {
+      $redirect .= '?postError=1&commentTooLong=1#comments';
+      header("Location: /" . str_replace(' ', '+', $redirect));
+      exit();
+    }
+        
+    $result = Model_Comment_Api::getInstance()->postComment($content, $author, $authorIp, $objectId, $objectType);
+    
+    if ($data) {
+      // data validation      
+      if (!$result) {
+        $redirect .= '?postError=1&dbError=1';
+      }
+      header("Location: /" . str_replace(' ', '+', $redirect . '#comments'));
+      exit();
+    }
+    else {
+      header("Location: /404.html", true, 404);
+      exit();
+    }
+  }
+  
+  public function viewAction()
+  {
+  }
+}
---
+++ tags/v0.48/library/Jkl/Og.php
@@ -0,0 +1,53 @@
+<?php
+/**
+* Jkl_Og
+*/
+class Jkl_Og
+{
+  private $_title;
+  private $_description;
+  private $_image;
+  private $_siteName;
+  
+  function __construct($siteName)
+  {
+    $this->_siteName = $siteName;
+  }
+  
+  public function setTitle($title)
+  {
+    $this->_title = $title;
+  }
+  
+  public function setDescription($description)
+  {
+    $this->_description = $description;
+  }
+  
+  public function setImage($image)
+  {
+    $this->_image = $image;
+  }
+  
+  public function echoMeta()
+  {
+    $ogMeta = '';
+    if (isset($this->_title)) {
+      $ogMeta .= '<meta property="og:title" content="' . str_replace('"', '&quot;', $this->_title) . '" />' . "\n";
+    }
+    
+    if (isset($this->_description)) {
+      $description = Jkl_Tools_String::trim_str($this->_description, 297);
+      $ogMeta .= '<meta property="og:description" content="' . str_replace('"', '&quot;', $description) . '" />' . "\n";
+    }
+    
+    if (isset($this->_image)) {
+      $ogMeta .= '<meta property="og:image" content="' . $this->_image . '" />' . "\n";
+    }
+    
+    $ogMeta .= '<meta property="og:site_name" content="' . $this->_siteName . '" />' . "\n";
+    
+    return $ogMeta;
+  }
+  
+}
