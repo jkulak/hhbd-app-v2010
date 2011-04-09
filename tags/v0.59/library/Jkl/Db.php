bdc8b608fff629138562f0003b3975431c313dd2
Jakub.kulak@gmail.com <Jakub.kulak@gmail.com@6c0f084b-7910-9a3f-d28b-cafd93553588>
2011-04-09 22:44:50 +0000

    creating tag v0.59

---
+++ tags/v0.59/library/Jkl/Db.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+* 
+*/
+class Jkl_Db extends Jkl_Cache
+{
+  private $_db;
+  private $_queryCount = 0;
+  
+  static private $_instance;
+  
+  /**
+   * Singleton instance
+   *
+   * @return Jkl_Db
+   */
+  public static function getInstance($adapter, $params)
+  {
+      if (null === self::$_instance) {
+          self::$_instance = new self($adapter, $params);
+      }
+
+      return self::$_instance;
+  }  
+  
+  function __construct($adapter, $params) {
+    $this->_db = Zend_Db::factory($adapter, $params);
+    parent::__construct();
+  }
+  
+  /*
+  * FechtAll with memcached support
+  */
+  public function fetchAll($query, $lifeTime = null)
+  {
+    $this->_queryCount++;
+
+    $cache = $this->_cache->load(md5($query));
+    if (empty($cache)) {
+
+      $result = $this->_db->fetchAll($query);
+      if ($lifeTime === null) {
+        $test = $this->_cache->save($result, md5($query));    
+      }
+      else
+      {
+        $test = $this->_cache->save($result, md5($query), array(), $lifeTime);
+      }
+      Zend_Registry::get('Logger')->info(md5($query) . ' - db: ' . $this->_queryCount . '. - ' . $query);
+    }
+    else {
+      $result = $cache;
+    }
+    return $result;
+  }
+  
+  /*
+  * Used for non cached queries (like UPDATE)
+  */
+  public function query($query)
+  {
+    $this->_queryCount++;
+    return $this->_db->query($query);
+  }
+  
+  public function getQueryCount()
+  {
+    return $this->_queryCount;
+  }
+  
+  static public function escape($value)
+  {
+    $search = array("\\", "\0", "\n", "\r", "\x1a", "'", '"', '%');
+    $replace = array("\\\\", "\\0", "\\n", "\\r", "\Z", "\'", '\"', '\%');
+    return str_replace($search, $replace, $value);
+  }
+  
+}
